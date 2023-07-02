b95f87c1cd42b70caa05eb3461ef046ac581156d
matvey <matveyka.bryantsev@mail.ru>
2023-07-02 13:20:59 +0700

    fsdfsdfs

--- javaScript/video.js
+++ javaScript/video.js
@@ -1,30 +1,31 @@
-let button = document.querySelector('.title__video-play');
-let modal = document.querySelector('.title__modal');
-let close = document.querySelector('.title__close');
+let button = document.querySelector(".title__video-play");
+let modal = document.querySelector(".title__modal");
+let close = document.querySelector(".title__close");
+let video = document.querySelector(".title__modal-video video");
 
-button.addEventListener('click', function () {
-    modal.style.display = 'flex';
-    disableScroll()
+button.addEventListener("click", function () {
+  modal.style.display = "flex";
+  video.play();
+  video.muted = true;
+  disableScroll();
 });
-close.addEventListener('click', function () {
-    modal.style.display = 'none';
-    enableScroll()
+close.addEventListener("click", function () {
+  modal.style.display = "none";
+  video.pause();
+  enableScroll();
 });
 
 function disableScroll() {
-    // Get the current page scroll position
-    scrollTop =
-        window.pageYOffset || document.documentElement.scrollTop;
-    scrollLeft =
-        window.pageXOffset || document.documentElement.scrollLeft,
-
-        // if any scroll is attempted,
-        // set this to the previous value
-        window.onscroll = function () {
-            window.scrollTo(scrollLeft, scrollTop);
-        };
+  // Get the current page scroll position
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  (scrollLeft = window.pageXOffset || document.documentElement.scrollLeft),
+    // if any scroll is attempted,
+    // set this to the previous value
+    (window.onscroll = function () {
+      window.scrollTo(scrollLeft, scrollTop);
+    });
 }
 
 function enableScroll() {
-    window.onscroll = function () { };
+  window.onscroll = function () {};
 }
